cabaf305ded1b35727d146382b31cafced5d9725
Marko <mpecic2010@gmail.com>
2019-01-25 12:57:30 -0700

    added routes and controllers

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,14 @@
 //Update the name of the controller below and rename the file.
-const template = require("../controllers/template.js")
+const users = require("../controllers/users.js")
+const games = require("../controllers/games.js")
+const reviews = require("../controllers/reviews.js")
+const comments = require("../controllers/comments.js")
+
+
 
 module.exports = (app) => {
-    app.get("/", template.index);
+    app.get("/users", users.index);
+    app.get('/games', games.index);
+    app.get('/reviews', reviews.index);
+    app.get('/comments', comments.index);
 }
