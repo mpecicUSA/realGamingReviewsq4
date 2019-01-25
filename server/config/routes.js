ec88911354c91347e2ac4c0340ecfef82252f0bb
unknown <lucasromero65@yahoo.com>
2019-01-25 15:29:05 -0600

    lucas pushing

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,16 +7,35 @@ const comments = require("../controllers/comments.js")
 
 
 module.exports = (app) => {
+
     // Get requests 
+
+    //USERS
     app.get("/users", users.getAll);
+
+    //GAMES
     app.get('/games', games.getAll);
+
+    //REVIEWS
     app.get('/reviews', reviews.getAll);
+
+    //COMMENTS
     app.get('/comments', comments.getAll);
 
+
     //Post requests
+
+    //USERS
     app.post('/users/addUser', users.addUser);
+
+    //REVIEWS
     app.post('/reviews/addReview', reviews.AddReview);
+
+    //GAMES
     app.post('/games/addGame', games.AddGame);
+    
+    //COMMENTS
     app.post('/comments/addComment', comments.AddComment);
+
 
 }
