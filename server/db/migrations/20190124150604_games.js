62fc99c69941b95aa31c36a4205af6eb4867df7a
Marko <mpecic2010@gmail.com>
2019-01-25 14:10:37 -0700

    add dependencies

--- server/db/migrations/20190124150604_games.js
+++ server/db/migrations/20190124150604_games.js
@@ -1,14 +1,14 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('games', table) => {
+    return knex.schema.createTable('games', (table) => {
         table.increments();
         table.string('title');
         table.integer('rating');
         table.text('description');
         table.timestapms(true, true)
-    }
+    })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('games')
+    return knex.schema.dropTable('games')
 };
