5f04016e6787fcc8fa61578ebc3d6c01010527bc
Marko <mpecic2010@gmail.com>
2019-01-29 18:09:05 -0700

    added knexfile.js back to git, created local.env shared with team, init elasticbeanstalk

---
+++ server/knexfile.js
@@ -0,0 +1,38 @@
+if(process.env.NODE_ENV !== "production")
+    require('dotenv').load({path: './local.env'});
+
+
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: {
+            database: process.env.RDS_DB,
+            host: process.env.RDS_HOST, 
+            user: process.env.RDS_USER, 
+            password: process.env.RDS_PASSWORD, 
+            port: process.env.RDS_PORT
+        },
+        migrations: {
+            directory: __dirname + '/db/migrations',
+        },
+        seeds: {
+            directory: __dirname + '/db/seeds',
+        },
+    },
+    production: {
+        client: 'pg',
+        connection: {
+            database: process.env.RDS_DB,
+            host: process.env.RDS_HOST, 
+            user: process.env.RDS_USER, 
+            password: process.env.RDS_PASSWORD, 
+            port: process.env.RDS_PORT
+        },
+        migrations: {
+            directory: __dirname + '/db/migrations',
+        },
+        seeds: {
+            directory: __dirname + '/db/seeds/production',
+        },
+    },
+};
