97318040bc97ac15131ed321b7cafadc42596951
Marko <mpecic2010@gmail.com>
2019-01-31 15:59:36 -0700

    renamed component to Navbarz as navbar is reserved, import requirments for react/ bootstrap, set up nav bar

--- client/real_gaming_reviews_frontend/src/App.js
+++ client/real_gaming_reviews_frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/Navbar.js'
+import Navbarz from './components/Navbar.js'
 import Footer from './components/Footer.js'
 import Home from './components/Home.js'
 import Login from './components/Login.js'
@@ -9,7 +9,6 @@ import IndividualGame from './components/Individual_game.js'
 import UserProfile from './components/User_profile.js'
 import CreateReview from './components/Create_review.js'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Button } from 'reactstrap'
 
 
 
@@ -21,7 +20,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <>
-          <Navbar />
+          <Navbarz />
 
           <Switch>
             <Route path="/" exact render={() => <Home />} />
--- client/real_gaming_reviews_frontend/src/components/Navbar.js
+++ client/real_gaming_reviews_frontend/src/components/Navbar.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Button,
+  Form
+} from 'reactstrap';
 
 
 
-
-
-class Navbar extends Component {
+class Navbarz extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
     render() {
       return (
-        <h1>Navbar</h1>
-      );
-    }
+        <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Real Gaming Reviews</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+              <Form className="form-inline my-2 my-lg-0">
+                <input className="form-control mr-sm-2" type="search" placeholder="Search Games" aria-label="Search"/>
+              </Form>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                {/* insert logic here IF token ?
+                userData == true ? <NavLink href='/profile'>Welcome back {DATA USER NAME} </NavLink> :
+                */}
+                <NavLink href="/login">Login</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
   }
+}
   
-  export default Navbar;
+  export default Navbarz;
