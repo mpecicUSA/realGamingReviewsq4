d36cb21b37419f508657b21d5a90363786aa5bdd
unknown <lucasromero65@yahoo.com>
2019-02-01 16:58:29 -0600

    individual game page is done minus styling

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,17 +5,10 @@ module.exports = {
     development: {
         client: 'pg',
         connection: {
-<<<<<<< HEAD
             database: "realGamingReviews",
             host: 'localhost',
             user: 'postgres', 
             password: 'root'
-=======
-            database: 'realGamingReviews',
-            host: 'localhost'
-            // ,user: 'postgres', 
-            // password: 'root'
->>>>>>> 3cd4be65f33665a6f70d2c2099a75044bf01530f
         },
         migrations: {
             directory: __dirname + '/db/migrations',
