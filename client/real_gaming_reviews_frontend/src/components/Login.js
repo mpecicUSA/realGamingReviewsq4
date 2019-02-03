486dc147f95b41b4263a05e81fa09225cdbac108
Nathan <nugnorberg@gmail.com>
2019-02-02 19:51:18 -0700

    component update

--- client/real_gaming_reviews_frontend/src/components/Login.js
+++ client/real_gaming_reviews_frontend/src/components/Login.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import { Container, Form, Button, Input, FormGroup, Label } from 'reactstrap';
 
+class Login extends Component{
 
-
-
-
-
-
-
-class Login extends Component {
-    render() {
-      return (
-        <h1>Login page here</h1>
-      );
-    }
+  state = {
+    email: "",
+    password: ""
   }
-  
-  export default Login;
+
+  onChange = ({target}) => {
+    this.setState({
+      [target.name]: target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.login(this.state, this.props.history)
+  }
+
+  render(){
+    return (
+      <Container>
+        <Form onSubmit={this.onSubmit} style={{display: "flex", alignItems: "center", justifyContent: "flex-end"}}>
+          {this.props.loginError && <p>Email and/or password is incorrect</p>}
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input onChange={this.onChange} value={this.state.email} name="email" type='email' placeholder='email' />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input onChange={this.onChange} value={this.state.password} name="password" type='password' placeholder='password' />
+          </FormGroup>
+          <Button>Sign in</Button>
+        </Form>
+      </Container>
+    )
+  }
+}
+
+export default Login
--- client/real_gaming_reviews_frontend/src/components/Register.js
+++ client/real_gaming_reviews_frontend/src/components/Register.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
-
-
-
-
-
+import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
 
 class Register extends Component {
-    render() {
-      return (
-        <h1>Register page here</h1>
-      );
-    }
+  state = {
+    email: "",
+    password: ""
   }
-  
+
+  onChange = ({target}) => {
+    this.setState({
+      [target.name]: target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+
+    this.props.addUser(this.state, this.props.history)
+  }
+
+  render() {
+
+    return (
+      <Container>
+        <Form onSubmit={this.onSubmit}>
+          {this.props.registerError && <span style={{color: "red"}}>Email already linked to an existing account</span>}
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input onChange={this.onChange} value={this.state.email} type="email" name="email" id="email" placeholder="Email" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input onChange={this.onChange} value={this.state.password} type="password" name="password" id="password" placeholder="Password" />
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
+      </Container>
+    )
+  }
+}
+
+
   export default Register;
