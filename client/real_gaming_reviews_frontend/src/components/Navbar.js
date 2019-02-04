b6653bda1a353d590652629a2f7524220ce16fca
Marko <mpecic2010@gmail.com>
2019-02-04 15:19:03 -0700

    navbar updates, search bar only on home page

--- client/real_gaming_reviews_frontend/src/components/Navbar.js
+++ client/real_gaming_reviews_frontend/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import {
 MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline
 } from "mdbreact";
-
+import { withRouter } from 'react-router-dom'
 
 const header = {
   styles: {
@@ -38,6 +38,7 @@ render() {
         </MDBNavbarBrand>
         <MDBNavbarToggler onClick={this.toggleCollapse} />
         <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+        {this.props.history.location.pathname === "/" ? <>
         <MDBNavbarNav center>
             <MDBNavItem>
               <MDBFormInline waves>
@@ -47,19 +48,29 @@ render() {
               </MDBFormInline>
             </MDBNavItem>
           </MDBNavbarNav>
+        </> : null}
+        
           <MDBNavbarNav right>
             <MDBNavItem >
               <MDBNavLink style={link.styles} to="/">Home</MDBNavLink>
             </MDBNavItem>
+          {
+            localStorage.getItem('token') ? <>
             <MDBNavItem>
               <MDBNavLink style={link.styles} to="/user/:id">Profile</MDBNavLink>
-            </MDBNavItem>
+            </MDBNavItem> 
+            <MDBNavItem>
+              <MDBNavLink style={link.styles} to="/user/logout">Logout</MDBNavLink>
+            </MDBNavItem> 
+            </>: <>
             <MDBNavItem>
               <MDBNavLink style={link.styles} to="/login">Login</MDBNavLink>
             </MDBNavItem>
             <MDBNavItem>
               <MDBNavLink style={link.styles} to="/register">Register</MDBNavLink>
             </MDBNavItem>
+            </>
+          }
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBNavbar>
@@ -67,6 +78,6 @@ render() {
   }
 }
 
-export default Navbarz;
+export default withRouter(Navbarz);
 
 // TODO Insert Logic for Profile or Login / Register icons in nav bar - PENDING token / login
